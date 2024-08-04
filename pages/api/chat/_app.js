dbafddc07529cbf9992ce47cb88b57fb673ade3c
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-08-04 17:34:33 -0400

    Create _app.js

---
+++ pages/api/chat/_app.js
@@ -0,0 +1,13 @@
+import '../public/styles/global.css';
+import Navbar from '../components/Navbar';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
